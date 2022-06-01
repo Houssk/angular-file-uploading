ef9392236ef436a2e76a2a54c3356321b467785b
Houssam <karrach.houssam@gmail.com>
2022-06-01 13:52:18 +0200

    add serverlink env

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -3,6 +3,8 @@ import {FileUploadService} from './file-upload.service';
 // @ts-ignore
 import {EngineFrameService} from "@app/ui/engine-frame/engine-frame.service";
 
+import {environment} from "@environments/environment";
+
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
@@ -37,11 +39,11 @@ export class FileUploadComponent implements OnInit {
     console.log(this.file);
     this.fileUploadService.upload(this.file, 'upload').subscribe( //this.path
       (event: any) => {
-        this.shortLink = `http://localhost:3000/${event.filename}`;
+        this.shortLink = `${environment.serverLink}${event.filename}`;
         console.log('event', event[1]);
 
         const size = event[0]['original_size']
-        let ratio = 200/size['width']; //set the displayed image width to 200mm
+        let ratio = 200 / size['width']; //set the displayed image width to 200mm
 
         this.response = event[0]['detection'];
         this.loading = false; // Flag variable
@@ -53,26 +55,26 @@ export class FileUploadComponent implements OnInit {
   }
 
   //OnClik of button Automatic detectoon
-  onDetection(){
+  onDetection() {
     this.loading = !this.loading;
     console.log(this.file);
     this.fileUploadService.upload(this.file, 'detection').subscribe( //this.path
       (event: any) => {
-        this.shortLink = `http://localhost:3000/${event.filename}`;
+        this.shortLink = `${environment.serverLink}${event.filename}`;
         console.log('event', event[1]);
 
         const size = event[0]['original_size']
-        let ratio = 200/size['width']; //set the displayed image width to 200mm
+        let ratio = 200 / size['width']; //set the displayed image width to 200mm
 
         this.response = event[0]['detection'];
         this.loading = false; // Flag variable
-  
-        this.engineFrameService.displayDetection(event[0]['detection']['big_troch'],size, ratio); //big_troch
-        this.engineFrameService.displayDetection(event[0]['detection']['little_troch'],size, ratio); 
-        this.engineFrameService.displayDetection(event[0]['detection']['bot_ax'],size, ratio); 
-        this.engineFrameService.displayDetection(event[0]['detection']['top_ax'],size, ratio); 
-        this.engineFrameService.displayDetection(event[0]['detection']['center'],size, ratio); 
-        this.engineFrameService.displayDetection(event[0]['detection']['corner'],size, ratio); 
+
+        this.engineFrameService.displayDetection(event[0]['detection']['big_troch'], size, ratio); //big_troch
+        this.engineFrameService.displayDetection(event[0]['detection']['little_troch'], size, ratio);
+        this.engineFrameService.displayDetection(event[0]['detection']['bot_ax'], size, ratio);
+        this.engineFrameService.displayDetection(event[0]['detection']['top_ax'], size, ratio);
+        this.engineFrameService.displayDetection(event[0]['detection']['center'], size, ratio);
+        this.engineFrameService.displayDetection(event[0]['detection']['corner'], size, ratio);
 
       }
     );
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  serverLink : 'http://nest.3d4you.org/'
 };
