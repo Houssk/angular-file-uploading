b3412495e209e029abf3b1a41cfafe9749c9d41c
Houssam <karrach.houssam@gmail.com>
2022-06-20 11:23:02 +0200

    refactoring process

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FileUploadService} from './file-upload.service';
-import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
+import {FormGroup, FormBuilder, FormArray, FormControl} from '@angular/forms';
 // @ts-ignore
 import {EngineFrameService} from "@app/ui/engine-frame/engine-frame.service";
 
@@ -35,11 +35,10 @@ export class FileUploadComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  verif(){
-    if(this.form.value['nbrHip']=='' || this.form.value['side']=='') {
+  verif() {
+    if (this.form.value['nbrHip'] == '' || this.form.value['side'] == '') {
       return true
-    }
-    else {
+    } else {
       return false
     }
   }
@@ -52,55 +51,39 @@ export class FileUploadComponent implements OnInit {
   // OnClick of button Upload
   onUpload() {
     this.loading = !this.loading;
-    console.log(this.file);
     this.fileUploadService.upload(this.file, 'upload').subscribe( //this.path
       (event: any) => {
         this.shortLink = `${environment.serverLink}/${event.filename}`;
-
-        console.log(environment.serverLink);
-        console.log('event', event[1]);
-
         const size = event[0]['original_size']
-
-        this.ratio = 200/size['width']; //set the displayed image width to 200mm
-
-
+        this.ratio = 200 / size['width']; //set the displayed image width to 200mm
         this.response = event[0]['detection'];
         this.loading = false; // Flag variable
         this.engineFrameService.loadImage(event[1], size, this.ratio);
         this.engineFrameService.displayCircle(event[0]['detection'], size, this.ratio);
-
         let circleRadius = 12 //12mm
-        this.scale = circleRadius/(event[0]['detection']['radius']*this.ratio)
+        this.scale = circleRadius / (event[0]['detection']['radius'] * this.ratio)
       }
     );
   }
 
-  //OnClik of button Automatic detectoon
+  //OnClik of button Automatic detection
   onDetection() {
     this.loading = !this.loading;
     console.log(this.file[0]);
     this.fileUploadService.detection(this.form, 'detection').subscribe( //this.path
       (event: any) => {
-
-        this.shortLink = `http://localhost:3000/${event.filename}`;
-        console.log('event', event[1]);
-
+        this.shortLink = `${environment.serverLink}/${event.filename}`;
         const size = event[0]['original_size']
         this.response = event[0];
         this.loading = false; // Flag variable
-  
-        this.engineFrameService.displayDetection(event[0]['detection']['big_troch'], size, this.ratio); //big_troch
-        this.engineFrameService.displayDetection(event[0]['detection']['little_troch'], size, this.ratio); 
-        this.engineFrameService.displayDetection(event[0]['detection']['bot_ax'], size, this.ratio); 
-        this.engineFrameService.displayDetection(event[0]['detection']['top_ax'], size, this.ratio); 
-        this.engineFrameService.displayDetection(event[0]['detection']['center'], size, this.ratio); 
-        this.engineFrameService.displayDetection(event[0]['detection']['corner'], size, this.ratio); 
-        
+        const arrayPoint = ['big_troch', 'little_troch', 'bot_ax', 'top_ax', 'center', 'corner'];
+        arrayPoint.forEach(point => {
+          this.engineFrameService.displayDetection(event[0]['detection'][point], size, this.ratio); //big_troch
+        })
         this.engineFrameService.onLandmarksDisplayCup(event[0]['detection']['center'], size, this.ratio, this.scale, event[0]['side'])
         this.engineFrameService.onLandmarksDisplayRod(event[0]['detection']['top_ax'], event[0]['detection']['bot_ax'], event[0]['detection']['center'], size, this.ratio, this.scale, event[0]['side'])
 
       }
-    ); 
+    );
   }
 }
--- src/app/ui/engine-frame/engine-frame.service.ts
+++ src/app/ui/engine-frame/engine-frame.service.ts
@@ -6,8 +6,7 @@ import {RendererService} from '@app/services/three/renderer.service';
 import {LightService} from '@app/services/three/light.service';
 import {SceneService} from '@app/services/three/scene.service';
 import {AxesHelper} from "three";
-import { Xliff2 } from '@angular/compiler';
-import { Utils } from '@app/shared/class/Utils';
+import {Utils} from '@app/shared/class/Utils';
 
 
 declare global {
@@ -94,7 +93,6 @@ export class EngineFrameService implements OnDestroy {
       this.ngZone.runOutsideAngular(() => {
         const width = this.canvas.offsetWidth;
         const height = this.canvas.offsetHeight;
-        console.log('width ', 'Height ', width, height)
         this.camera.left = width / -2;
         this.camera.right = width / 2;
         this.camera.top = height / 2;
@@ -110,73 +108,59 @@ export class EngineFrameService implements OnDestroy {
     const texture = new THREE.TextureLoader().load(pathLink);
 
     const box = new THREE.Mesh(
-      new THREE.BoxGeometry(size['width']*ratio, size['height']*ratio, 1),
+      new THREE.BoxGeometry(size['width'] * ratio, size['height'] * ratio, 1),
       new THREE.MeshBasicMaterial({
         map: texture
       }));
+    box.renderOrder = 3;
     this.scene.add(box);
   }
 
 
-
-  public displayCircle(points, size, ratio){
-    
-    let new_radius = points['radius']*ratio
-  
-    const circle = new THREE.Mesh( 
-      new THREE.RingGeometry( new_radius, new_radius + 0.5, 20), 
+  public displayCircle(points, size, ratio) {
+    let new_radius = points['radius'] * ratio
+    const circle = new THREE.Mesh(
+      new THREE.RingGeometry(new_radius, new_radius + 0.5, 20),
       new THREE.MeshNormalMaterial()
     );
- 
-    let middle_x = size['width']/2
-    let middle_y = size['height']/2
-
-    let new_x = points['x']*ratio - middle_x*ratio
-    let new_y = - points['y']*ratio + middle_y*ratio
-
+    let middle_x = size['width'] / 2
+    let middle_y = size['height'] / 2
+    let new_x = points['x'] * ratio - middle_x * ratio
+    let new_y = -points['y'] * ratio + middle_y * ratio
     circle.position.set(new_x, new_y, 1)
-
-    this.scene.add( circle );
+    this.scene.add(circle);
   }
 
 
-
-  public displayDetection(landmark, size, ratio){
-
+  public displayDetection(landmark, size, ratio) {
     const point = new THREE.Mesh(
-      new THREE.SphereGeometry( 1, 5, 5 ),
-      new THREE.MeshBasicMaterial({ color: 0xff0000 })
-      );
-    
-    let middle_x = size['width']/2
-    let middle_y = size['height']/2
-
-    let new_x = landmark['x']*ratio - middle_x*ratio
-    let new_y = - landmark['y']*ratio + middle_y*ratio
-    
+      new THREE.SphereGeometry(1, 5, 5),
+      new THREE.MeshBasicMaterial({color: 0xff0000})
+    );
+    let middle_x = size['width'] / 2
+    let middle_y = size['height'] / 2
+    let new_x = landmark['x'] * ratio - middle_x * ratio
+    let new_y = -landmark['y'] * ratio + middle_y * ratio
     point.position.set(new_x, new_y, 0)
-    
     this.scene.add(point)
-
   }
 
-  
-  public onLandmarksDisplayCup(landmark, size, ratio, scale, side: string){
+
+  public onLandmarksDisplayCup(landmark, size, ratio, scale, side: string) {
     let pathLink: string
-    pathLink='undefined'
+    pathLink = 'undefined'
 
     // wPix=1136	hPix=915	wCm=9.62	hCm=7.75
-    if (side=='right'){
+    if (side == 'right') {
       pathLink = `./assets/images/Cup49_R.png`
-    }
-    else if (side=='left'){
+    } else if (side == 'left') {
       pathLink = `./assets/images/Cup49.png`
     }
-    
+
     const texture = new THREE.TextureLoader().load(pathLink)
 
-    let new_w = 96.2/scale
-    let new_h = 77.5/scale
+    let new_w = 96.2 / scale
+    let new_h = 77.5 / scale
 
     const cup = new THREE.Mesh(
       new THREE.BoxGeometry(new_w, new_h, 1),
@@ -184,35 +168,34 @@ export class EngineFrameService implements OnDestroy {
         map: texture,
         transparent: true
       }));
+    cup.renderOrder = 1;
+    let middle_x = size['width'] / 2
+    let middle_y = size['height'] / 2
 
-    let middle_x = size['width']/2
-    let middle_y = size['height']/2
-  
-    let new_x = landmark['x']*ratio - middle_x*ratio
-    let new_y = - landmark['y']*ratio + middle_y*ratio
-      
+    let new_x = landmark['x'] * ratio - middle_x * ratio
+    let new_y = -landmark['y'] * ratio + middle_y * ratio
+
     cup.position.set(new_x, new_y, 0)
 
     this.scene.add(cup);
   }
 
 
-  public onLandmarksDisplayRod(topAx, botAx, center, size, ratio, scale, side: string){
+  public onLandmarksDisplayRod(topAx, botAx, center, size, ratio, scale, side: string) {
     let pathLink: string
-    pathLink='undefined'
+    pathLink = 'undefined'
     let pos_x = 0
     let pos_y = 0
     let rot = 0
     let axDiaX = 0
 
     // wPix=215	wCm=5.46	hPix=800	hCm=20.32 rapportmm/px=O.254
-    if (side=='right'){
+    if (side == 'right') {
       pathLink = `./assets/images/hype_scs_T3_R.png` //ptmecahaut x=81.5 y=371 //axDiaph x=-80
       pos_x = 81.5 //can be commented
       pos_y = 371
       axDiaX = -80
-    }
-    else if (side=='left'){
+    } else if (side == 'left') {
       pathLink = `./assets/images/hype_scs_T3.png` //ptmecahaut x=-79.5 y=371 //axDiaph x=78
       pos_x = -79.5
       pos_y = 371
@@ -221,8 +204,8 @@ export class EngineFrameService implements OnDestroy {
 
     const texture = new THREE.TextureLoader().load(pathLink)
 
-    let new_w = 54.6/scale
-    let new_h = 203.2/scale
+    let new_w = 54.6 / scale
+    let new_h = 203.2 / scale
 
     const rod = new THREE.Mesh(
       new THREE.BoxGeometry(new_w, new_h, 1),
@@ -232,8 +215,8 @@ export class EngineFrameService implements OnDestroy {
       }));
 
     //define the middle of the image wich will be the point O of the application world
-    let middle_x = size['width']/2
-    let middle_y = size['height']/2
+    let middle_x = size['width'] / 2
+    let middle_y = size['height'] / 2
 
     //Find the directing coef to find the angle between the detected axe and the vertical line
     const x1 = topAx['x']
@@ -241,37 +224,37 @@ export class EngineFrameService implements OnDestroy {
     const x2 = botAx['x']
     const y2 = botAx['y']
     let a = 0
-    if ((x1-x2) != 0) {
-      a = (y2-y1)/(x2-x1) }
-    else {
-      a = (y2-y1/0.001) }
-
-    //Find the final angle 
-    if (topAx['x']>botAx['x']){
-      rot= Math.PI/2
+    if ((x1 - x2) != 0) {
+      a = (y2 - y1) / (x2 - x1)
+    } else {
+      a = (y2 - y1 / 0.001)
     }
-    else {
-      rot= - Math.PI/2
+
+    //Find the final angle
+    if (topAx['x'] > botAx['x']) {
+      rot = Math.PI / 2
+    } else {
+      rot = -Math.PI / 2
     }
     const angle = Math.atan(a) + rot
 
     //define the position of the topAx point in the application world
-    let topAx_x = topAx['x']*ratio - middle_x*ratio
-    let topAx_y = - topAx['y']*ratio + middle_y*ratio
+    let topAx_x = topAx['x'] * ratio - middle_x * ratio
+    let topAx_y = -topAx['y'] * ratio + middle_y * ratio
 
-    //define the position to the center of the rod layer before the rotation 
-    let x_before_rotate = topAx_x - (axDiaX*0.254/scale)
-    let y_before_rotate = topAx_y - (pos_y*0.254/scale)
+    //define the position to the center of the rod layer before the rotation
+    let x_before_rotate = topAx_x - (axDiaX * 0.254 / scale)
+    let y_before_rotate = topAx_y - (pos_y * 0.254 / scale)
 
     //define the point and the axis around which the image will rotate
-    const point_rot = new THREE.Vector3( topAx_x, topAx_y, 0 );
+    const point_rot = new THREE.Vector3(topAx_x, topAx_y, 0);
     const axis_rot = new THREE.Vector3(0, 0, 1)
 
     // TO DELETE
     const point = new THREE.Mesh(
-      new THREE.SphereGeometry( 1, 5, 5 ),
-      new THREE.MeshBasicMaterial({ color: 0x0000ff })
-      );
+      new THREE.SphereGeometry(1, 5, 5),
+      new THREE.MeshBasicMaterial({color: 0x0000ff})
+    );
     point.position.set(x_before_rotate, y_before_rotate, 0)
     this.scene.add(point);
     //
@@ -280,11 +263,11 @@ export class EngineFrameService implements OnDestroy {
     Utils.rotateAroundWorldAxis(rod, point_rot, axis_rot, -angle)
 
     //reset position according to the rotation
-    let dist = (center['y']-topAx['y'])*ratio
-    let x_after_rotate = x_before_rotate + Math.tan(angle)*dist
+    let dist = (center['y'] - topAx['y']) * ratio
+    let x_after_rotate = x_before_rotate + Math.tan(angle) * dist
     let y_after_rotate = y_before_rotate - dist
     rod.position.set(x_after_rotate, y_after_rotate, 0)
-
+    rod.renderOrder = 2;
     this.scene.add(rod);
   }
 
